2a937756c0bd6d2eafc92b0538d8c571848c64c0
m_a_st2 <m_a_st2@yahoo.com>
2020-11-16 14:23:24 +0330

    Seed AppIdentityDb to add administrator's username and password

---
+++ src/MehdiWebApp.Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehdiWebApp.Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminRoleName = "Administrator";
+            await roleManager.CreateAsync(new IdentityRole(adminRoleName));
+
+            // TODO: Add admin username and password by using "User Secrets Manager"
+            string adminUserName = "admin@mng.com";
+            string adminPassword = "Aa@123456";
+            var adminUser = new ApplicationUser { UserName = adminUserName };
+            await userManager.CreateAsync(adminUser, adminPassword);
+
+            adminUser = await userManager.FindByNameAsync(adminUserName);
+            await userManager.AddToRoleAsync(adminUser, adminRoleName);
+        }
+    }
+}
