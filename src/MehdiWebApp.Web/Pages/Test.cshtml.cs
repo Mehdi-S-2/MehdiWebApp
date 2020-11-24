3f1f24c344b1eb6135e950842e38f79612c5b254
Mehdi SH <Mehdi SH@DESKTOP-9ID5OFU>
2020-11-24 22:48:47 +0330

    Add a test page

---
+++ src/MehdiWebApp.Web/Pages/Test.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MehdiWebApp.Web.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
