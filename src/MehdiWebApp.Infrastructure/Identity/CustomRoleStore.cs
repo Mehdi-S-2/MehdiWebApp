f655f1f0403ea771a43ff3c024f83c14cb673e93
Mehdi SH <Mehdi SH@DESKTOP-9ID5OFU>
2020-12-25 11:24:48 +0330

    Add preliminary and incomplete  CustomUserStore.cs and CustomRoleStore.cs

---
+++ src/MehdiWebApp.Infrastructure/Identity/CustomRoleStore.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MehdiWebApp.Infrastructure.Identity
+{
+    public class CustomRoleStore<TRole, TContext, TKey, TUserRole, TRoleClaim>: RoleStore<TRole, TContext, TKey, TUserRole, TRoleClaim>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+        where TContext : DbContext
+        where TUserRole : IdentityUserRole<TKey>, new()
+        where TRoleClaim : IdentityRoleClaim<TKey>, new()
+    {
+        public CustomRoleStore(TContext context, IdentityErrorDescriber describer = null) : base(context, describer) { }
+    }
+}
---
+++ src/MehdiWebApp.Infrastructure/Identity/CustomUserStore.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MehdiWebApp.Infrastructure.Identity
+{
+    public class CustomUserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, TUserLogin, TUserToken, TRoleClaim> : UserStore<TUser, TRole, TContext, TKey, TUserClaim, TUserRole, TUserLogin, TUserToken, TRoleClaim>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TContext : DbContext
+        where TKey : IEquatable<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>, new()
+        where TUserRole : IdentityUserRole<TKey>, new()
+        where TUserLogin : IdentityUserLogin<TKey>, new()
+        where TUserToken : IdentityUserToken<TKey>, new()
+        where TRoleClaim : IdentityRoleClaim<TKey>, new()
+    {
+        public CustomUserStore(TContext context, IdentityErrorDescriber describer = null) : base(context, describer) { }
+    }
+}
