798a73fd8594eceb3a8d777f8b08aa2e54345bff
Mehdi SH <Mehdi SH@DESKTOP-9ID5OFU>
2020-12-28 23:54:38 +0330

    Add a responsive layout as the default layout

---
+++ src/MehdiWebApp.Web/wwwroot/js/layout-default.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    function responsiveView() {
+        var wSize = $(window).width();
+        if (wSize < 768) {
+            $('#main-container').addClass('sidebar-closed');
+        }
+
+        if (wSize >= 768) {
+            $('#main-container').removeClass('sidebar-closed');
+        }
+    }
+
+    $(window).on('load', responsiveView);
+    $(window).on('resize', responsiveView);
+
+    $('#sidebar-toggle-box').click(function () {
+        $('#main-container').toggleClass("sidebar-closed");
+    });
+});
