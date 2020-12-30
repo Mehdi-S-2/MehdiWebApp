b9c897a0fd9037c88998b9e76224163356fddae5
m_a_st2 <m_a_st2@yahoo.com>
2020-12-30 14:21:34 +0330

    Add register page

---
+++ src/MehdiWebApp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using MehdiWebApp.Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace MehdiWebApp.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+
+        public RegisterModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<RegisterModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "(لطفا نام کاربری را مشخص کنید)")]
+            [Display(Name = "نام کاربری")]
+            public string UserName { get; set; }
+
+            [Required(ErrorMessage = "(لطفا کلمه عبور را مشخص کنید)")]
+            [StringLength(100, ErrorMessage = "فیلد {0} باید حداقل {2} و حداکثر {1} کاراکتر داشته باشد.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "کلمه عبور")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "تکرار کلمه عبور")]
+            [Compare("Password", ErrorMessage = "کلمه عبور با تکرار آن متفاوت است!")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = Input.UserName };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
